0b52388e07745113452ced61e1fbc6db18f58b9f
stone <stoneUoU@163.com>
2024-04-03 14:54:55 +0800

    fix:1.增加方法

--- Hi-Helper-Module/Classes/HiToast/HiHudToast.swift
+++ Hi-Helper-Module/Classes/HiToast/HiHudToast.swift
@@ -9,16 +9,18 @@ import MBProgressHUD
 
 public class HiHudToast {
     public static func showHUD(ctrl:UIViewController) {
-        let hud = MBProgressHUD.showAdded(to: ctrl.view, animated: true)
+        guard let view = UIApplication.shared.delegate?.window ?? ctrl.view else { return };
+        let hud = MBProgressHUD.showAdded(to: view, animated: true)
         hud.mode = MBProgressHUDMode.indeterminate
     }
     public static func show(ctrl:UIViewController,_ message: String) {
-    
-        let hud = MBProgressHUD.showAdded(to: ctrl.view, animated: true)
+        guard let view = UIApplication.shared.delegate?.window ?? ctrl.view else { return };
+        let hud = MBProgressHUD.showAdded(to:  view, animated: true)
         hud.mode = MBProgressHUDMode.indeterminate
         hud.label.text = message;
     }
     public static func hideHUD(ctrl:UIViewController) {//.hideAllHUDs(for: ctrl.view, animated: true)
-        MBProgressHUD.hide(for: ctrl.view, animated: true)
+        guard let view = UIApplication.shared.delegate?.window ?? ctrl.view else { return };
+        MBProgressHUD.hide(for: view, animated: true)
     }
 }
---
+++ HiCategory/UIImage+NilImage.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+NilImage.h
+//  Hi-Helper-Module
+//
+//  Created by stone on 2024/4/3.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+//@interface UIImage (NilImage)
+//
+//@end
+
+NS_ASSUME_NONNULL_END
